8601ae59d53ac283fa69fdb85c9d020bf7de56e5
SHRATEEK JAISWAL <SHRATEEK JAISWAL@LAPTOP-S917CSU6>
2024-07-03 11:29:05 +0530

    Test commit

---
+++ Test/src/main/java/Testing/Test/Temp.java
@@ -0,0 +1,10 @@
+package Testing.Test;
+
+public class Temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+}
---
+++ Test/src/main/java/Testing/Test/Testing.java
@@ -0,0 +1,14 @@
+package Testing.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		}
+	}
