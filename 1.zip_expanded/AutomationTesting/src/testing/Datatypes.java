d6f959a4e6546c234ece69e66932bf77849b85d2
SHRATEEK JAISWAL <SHRATEEK JAISWAL@LAPTOP-S917CSU6>
2024-07-03 15:07:17 +0530

    Automation testing

---
+++ 1.zip_expanded/AutomationTesting/src/testing/Datatypes.java
@@ -0,0 +1,26 @@
+package testing;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+int b=30;
+int c=40;
+int d=a+b+c;
+System.out.println("Sum is: " + (a+b+c));
+
+double e=25.147;
+char ch= 'C';
+String s="Welcome";
+boolean bo= true;
+System.out.println(e);
+System.out.println(ch);
+System.out.println(s);
+System.out.println(bo);
+System.out.println(d);
+
+
+	}
+
+}
